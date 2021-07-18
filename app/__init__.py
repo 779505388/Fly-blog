7dbf2f3628c440cdf5118eb7f4395ca2076f50dc
779505388 <45198849+779505388@users.noreply.github.com>
2021-07-18 23:52:01 +0800

    优化app初始化

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,9 @@ from views.admin.admin import admin
 from extension import config_init
 from api.api_v1 import api_v1
 from api.api_v2 import api_v2
-
+from logging.handlers import TimedRotatingFileHandler
+import logging
+from tool import getCategory, getItem, getInfo
 
 def create_app(config):
     app = Flask(__name__, template_folder='./templates/',
@@ -14,6 +16,18 @@ def create_app(config):
     app.register_blueprint(api_v1)
     app.register_blueprint(api_v2)
     app.config.from_object(config)
+
+    # 全局变量，获取文章分类
+    app.add_template_global(getCategory, 'getCategory')
+    app.add_template_global(getItem, 'getItem')
+    app.add_template_global(getInfo, 'getInfo')
     config_init(app)
 
+    formatter = logging.Formatter(
+        "[%(asctime)s][%(filename)s:%(lineno)d][%(levelname)s][%(thread)d] - %(message)s")
+    handler = TimedRotatingFileHandler(
+        "FlyBlog.log", when="D", interval=15, backupCount=15,
+        encoding="UTF-8", delay=False, utc=True)
+    app.logger.addHandler(handler)
+    handler.setFormatter(formatter)
     return app
--- serve.py
+++ serve.py
@@ -1,24 +1,13 @@
 from app import create_app
 from config import DevConfig
 from flask_cors import CORS
-from logging.handlers import TimedRotatingFileHandler
-import logging
-from tool import getCategory, getItem, getInfo
+
 
 app = create_app(DevConfig)
 app.jinja_env.auto_reload = True
-# 全局变量，获取文章分类
-app.add_template_global(getCategory, 'getCategory')
-app.add_template_global(getItem, 'getItem')
-app.add_template_global(getInfo, 'getInfo')
+
 CORS(app, resources={
      r"/api/*": {"origins": "*"}}, supports_credentials=True)
 if __name__ == "__main__":
-    formatter = logging.Formatter(
-        "[%(asctime)s][%(filename)s:%(lineno)d][%(levelname)s][%(thread)d] - %(message)s")
-    handler = TimedRotatingFileHandler(
-        "FlyBlog.log", when="D", interval=15, backupCount=15,
-        encoding="UTF-8", delay=False, utc=True)
-    app.logger.addHandler(handler)
-    handler.setFormatter(formatter)
+
     app.run(host="0.0.0.0", port=5000)
